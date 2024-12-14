c9a534b78406195a391ffc4ddce303f457fc105c
RonZatkovsky <167799853+RonZatkovsky@users.noreply.github.com>
2024-12-14 00:28:07 +0000

    done

--- src/models.py
+++ src/models.py
@@ -7,12 +7,41 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+    id=Column(Integer,primary_key=True)
+    user_name=Column(String(250), nullable=False)
+    email=Column(String(250),nullable=False)
+
+    def __ref__(self):
+        return f'<User {self.person}>'
+    
+    def serialize(self):
+        return {
+            'id':self.id,
+            'user_name':self.user_name,
+            'email':self.email,
+        }
+    
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    first_name=Column(String(250),nullable=False)
+    last_name=Column(String(250),nullable=False)
+    user_id=Column(Integer,ForeignKey('user.id'),nullable=False)
+    user=relationship(User)
+
+    def __ref__(self):
+        return f'<Person {self.user}>'
+    
+    def serialize(self):
+        return {
+            'id':self.id,
+            'first_name':self.first_name,
+            'last_name':self.last_name,
+        }
 
 class Address(Base):
     __tablename__ = 'address'
@@ -22,50 +51,131 @@ class Address(Base):
     street_name = Column(String(250))
     street_number = Column(String(250))
     post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
+    person_id = Column(Integer, ForeignKey('person.id'),nullable=False)
     person = relationship(Person)
 
-    def to_dict(self):
-        return {}
+    def __ref__(self):
+        return f'<Address {self.person}>'
+    
+    def serialize(self):
+        return {
+            'id':self.id,
+            'street_name':self.street_name,
+            'street_number':self.street_number,
+            'post_code':self.post_code,
+            'person_id':self.person_id,
+            'name':self.name,
+            'person':self.person,
+        }
 
-class User(Base):
-    __tablename__ = 'user'
-    ID=Column(Integer,primary_key=True)
-    user_name=Column(String(250))
-    first_name=Column(String(250))
-    last_name=Column(String(250))
-    email=Column(String(250))
 
-class Follower(Base):
-    __tablename__ = 'follower'
-    ID=Column(Integer,primary_key=True)
-    user_from_id=Column(Integer,ForeignKey('user.ID'))
-    user_to_id=Column(Integer,ForeignKey('user.ID'))
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id=Column(Integer,primary_key=True)
+    user_id=Column(Integer,ForeignKey('user.id'),nullable=False)
+    population=Column(Integer,nullable=False)
+    climate=Column(String,nullable=False)
+
     user=relationship(User)
 
-class Post(Base):
-    __tablename__ = 'post'
-    ID=Column(Integer,primary_key=True)
-    user_id=Column(Integer,ForeignKey('user.ID'))
+    def __ref__(self):
+        return f'<Planets {self.user}>'
+    
+    def serialize(self):
+        return {
+            'id':self.id,
+            'user_id':self.user_id,
+            'population':self.population,
+            'climate':self.climate,
+        }
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id=Column(Integer,primary_key=True)
+    user_id=Column(Integer,ForeignKey('user.id'),nullable=False)
+    name=Column(Integer,nullable=False)
+    race=Column(String,nullable=False)
+    description=Column(String,nullable=False)
     user=relationship(User)
+
+    def __ref__(self):
+        return f'<Characters {self.user}>'
+    
+    def serialize(self):
+        return {
+            'id':self.id,
+            'user_id':self.user_id,
+            'name':self.name,
+            'race':self.race,
+            'description':self.description,
+        }
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id=Column(Integer,primary_key=True)
+    user_id=Column(Integer,ForeignKey('user.id'),nullable=False)
+    planet_id=Column(Integer,ForeignKey('planets.id'),nullable=False)
+    character_id=Column(Integer,ForeignKey('characters.id'),nullable=False)
+    user=relationship(User)
+    planets=relationship(Planets)
+    characters=relationship(Characters)
+    
+    def __ref__(self):
+        return f'<Favorites {self.user}>'
+    
+    def serialize(self):
+        return {
+            'id':self.id,
+            'user_id':self.user_id,
+            'planet_id':self.planet_id,
+            'character_id':self.character_id,
+        }
 
 class Comment(Base):
     __tablename__ = 'comment'
-    ID=Column(Integer,primary_key=True)
+    id=Column(Integer,primary_key=True)
     comment_text=Column(String(250))
-    author_id=Column(Integer,ForeignKey('user.ID'))
-    post_id=Column(Integer,ForeignKey('post.ID'))
+    author_id=Column(Integer,ForeignKey('user.id'),nullable=False)
+    planet_id=Column(Integer,ForeignKey('planets.id'),nullable=False)
+    character_id=Column(Integer,ForeignKey('characters.id'),nullable=False)
     user=relationship(User)
-    post=relationship(Post)
+    planets=relationship(Planets)
+    characters=relationship(Characters)
+
+    def __ref__(self):
+        return f'<Comment {self.user}>'
+    
+    def serialize(self):
+        return {
+            'id':self.id,
+            'author_id':self.author_id,
+            'planet_id':self.planet_id,
+            'character_id':self.character_id,
+            'comment_text':self.comment_text
+        }
 
 class Media(Base):
     __tablename__ = 'media'
-    ID=Column(Integer,primary_key=True)
-    type=Column(String(250))
-    url=Column(String(250))
-    post_id=Column(Integer,ForeignKey('post.ID'))
-    post=relationship(Post)
+    id=Column(Integer,primary_key=True)
+    type=Column(String(250),nullable=False)
+    url=Column(String(250),nullable=False)
+    planet_id=Column(Integer,ForeignKey('planets.id'),nullable=False)
+    character_id=Column(Integer,ForeignKey('characters.id'),nullable=False)
+    planets=relationship(Planets)
+    characters=relationship(Characters)
 
+    def __ref__(self):
+        return f'<Media {self.id}>'
+    
+    def serialize(self):
+        return {
+            'id':self.id,
+            'type':self.type,
+            'planet_id':self.planet_id,
+            'character_id':self.character_id,
+            'url':self.url
+        }
 ## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
